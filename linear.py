f6e19369d63e34ff03309004ad463dddae551a07
MasaYan24 <m.takahashi.public+gitkraken@gmail.com>
2021-07-10 13:34:13 +0900

    update

--- linear.py
+++ linear.py
@@ -1,4 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+xmax = 1.0
+sample_size = 50
+err_sigma = 0.1
+alpha = 1.03
+beta = 0.0
 
+np.random.seed(1)
+x = xmax * np.random.rand(sample_size)
+err = err_sigma * np.random.rand(sample_size)
+y = alpha * x + beta + err
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.grid(True)
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.plot(x, y, "o")
+plt.show()
